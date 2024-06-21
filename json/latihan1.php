e022455959f161a9000c311a3e7eefe443c39a74
IbrahimHussein01 <shadamhussein11@gmail.com>
2024-06-21 23:12:16 +0700

    first init

---
+++ json/latihan1.php
@@ -0,0 +1,17 @@
+<?php
+// $mahasiswa = [
+//     [
+//         "nama" => "Ibrahim",
+//         "nip" => "2117020052",
+//         "email" => "ibrahim@gmail.com",
+//     ],
+//     [
+//         "nama" => "Ibrahim",
+//         "nip" => "2117020052",
+//         "email" => "ibrahim@gmail.com",
+//     ],
+// ];
+
+$data = json_encode($mahasiswa);
+echo $data;
+?> 
